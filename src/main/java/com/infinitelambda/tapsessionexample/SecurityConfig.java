93b77ce928c874f0e501ceab0f7edf0fd066a387
gabor.csuvik <gabor.csuvik@infinitelambda.com>
2022-01-30 23:50:31 +0100

    Fix actuator issues in healthcheck

--- src/main/java/com/infinitelambda/tapsessionexample/SecurityConfig.java
+++ src/main/java/com/infinitelambda/tapsessionexample/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .httpBasic().and()
                 .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated();
     }
 
